842f960ddd424b46cce87514d6c00b265a7fabb7
charles <charles@charless-MacBook-Pro.local>
2017-04-26 12:10:14 -0500

    voteup & votedown functionality

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -21,4 +21,33 @@ app.controller('HomeController', function(HomeService,$http) {
         });
       }
 
+      ctrl.createView=function(data){
+        var obj ={video_id:data.id};
+
+        console.log("inside createView",obj);
+        HomeService.createView(obj).then(function(data){
+          console.log("createView return data",data);
+          ctrl.getAllVideos();
+        });
+
+      }
+
+      ctrl.voteUp=function(data){
+        var obj={video_id:data.id,opinion:1}
+        console.log("inside voteUp",obj);
+        HomeService.voteUp(obj).then(function(data){
+          console.log("voteUp return data",data);
+          ctrl.getAllVideos();
+        });
+      }
+
+      ctrl.voteDown=function(data){
+        var obj={video_id:data.id,opinion:-1}
+        console.log("inside voteUp",obj);
+        HomeService.voteDown(obj).then(function(data){
+          console.log("voteDown return data",data);
+          ctrl.getAllVideos();
+        });
+      }
+
 });
--- public/scripts/home.service.js
+++ public/scripts/home.service.js
@@ -31,4 +31,54 @@ console.log(" inside HomeService ",$http.defaults.headers.common);
       });
   };
 
+  this.createView = function(data){
+    var jsonObj=JSON.stringify(data);
+    console.log("addVideo json object",jsonObj);
+    return  $http({
+        url: 'https://proofapi.herokuapp.com/views',
+        method: 'POST',
+        data: jsonObj
+      }).then(function(response){
+        console.log(response.data);
+        return response.data;
+
+      },function(error){
+        console.log(error);
+      });
+  };
+
+  this.voteUp = function(data){
+    var jsonObj=JSON.stringify(data);
+    console.log("voteUp json object",jsonObj);
+    return  $http({
+        url: 'https://proofapi.herokuapp.com/votes',
+        method: 'POST',
+        data: jsonObj
+      }).then(function(response){
+        console.log(response.data);
+        return response.data;
+
+      },function(error){
+        console.log(error);
+      });
+  };
+
+  this.voteDown = function(data){
+    var jsonObj=JSON.stringify(data);
+    console.log("voteDown json object",jsonObj);
+    return  $http({
+        url: 'https://proofapi.herokuapp.com/votes',
+        method: 'POST',
+        data: jsonObj
+      }).then(function(response){
+        console.log(response.data);
+        return response.data;
+
+      },function(error){
+        console.log(error);
+      });
+  };
+
+
+
 });
